cddecf65ac5ec9f1f42599681ebd806598ea5bc0
mirkocaforio <mirkocaforio2002@gmail.com>
2024-04-19 20:38:16 +0200

    Added getter and setter with lombok for AuthenticationResponseDTO

--- src/main/java/it/unisalento/pasproject/authservice/dto/AuthenticationResponseDTO.java
+++ src/main/java/it/unisalento/pasproject/authservice/dto/AuthenticationResponseDTO.java
@@ -1,21 +1,16 @@
 package it.unisalento.pasproject.authservice.dto;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 // Risposta che contiene il token (JsonWebToken) di autenticazione
+@Getter
+@Setter
 public class AuthenticationResponseDTO {
     private String jwt;
 
     public AuthenticationResponseDTO(String jwt) {
         this.jwt = jwt;
     }
-
-    public String getJwt() {
-        return jwt;
-    }
-
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
-    }
-
-
 }
