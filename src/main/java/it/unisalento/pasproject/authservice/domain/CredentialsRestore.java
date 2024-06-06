ec7d8cb7836adc617dcf9671b4fcd5a8d2721925
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-06-06 09:21:25 +0200

    fix: ci/cd

--- src/main/java/it/unisalento/pasproject/authservice/domain/CredentialsRestore.java
+++ src/main/java/it/unisalento/pasproject/authservice/domain/CredentialsRestore.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDateTime;
 
 @Getter
