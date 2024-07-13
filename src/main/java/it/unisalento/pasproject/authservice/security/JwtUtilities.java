c3b58a11a82eecdc8933a3704c710345d78c0a40
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-07-13 12:07:37 +0200

    fix: Hot fix security

--- src/main/java/it/unisalento/pasproject/authservice/security/JwtUtilities.java
+++ src/main/java/it/unisalento/pasproject/authservice/security/JwtUtilities.java
@@ -3,6 +3,7 @@ package it.unisalento.pasproject.authservice.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-import static it.unisalento.pasproject.authservice.security.SecurityConstants.JWT_SECRET;
 
 @Service
 public class JwtUtilities {
-    private final Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    private final Key key;
+
+    @Autowired
+    public JwtUtilities(SecurityConstants securityConstants) {
+        key = Keys.hmacShaKeyFor(securityConstants.getJWT_SECRET().getBytes());
+    }
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
